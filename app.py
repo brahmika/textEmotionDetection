9478f5d08b085e33dfa01de640c58af78364ead9
Brahmika Shree Balaji <brahmika30@gmail.com>
2024-07-02 19:45:51 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,54 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import altair as alt
+import joblib
+
+pipe_lr = joblib.load(open("model/text_emotion.pkl", "rb"))
+emotions_dir = {"anger": "😡", "disgust": "🤢", "fear": "😨", "happy": "😀", "joy": "😁", "neutral": "😐", "sad": "🥲","sadness":"😔" , "shame": "😳", "surprise": "😧"}
+
+
+def predict_emotions(docx):
+    results = pipe_lr.predict([docx])
+    return results[0]
+
+
+def get_prediction_prob(docx):
+    results = pipe_lr.predict_proba([docx])
+    return results
+
+
+def main():
+    st.title("Text Emotion Detection")
+    st.subheader("Detect Emotions In Text")
+
+    with st.form(key='my_form'):
+        raw_text = st.text_area("Type Here")
+        submit_text = st.form_submit_button(label="Submit")
+
+    if submit_text:
+        col1, col2 = st.columns(2)
+
+        prediction = predict_emotions(raw_text)
+        probability = get_prediction_prob(raw_text)
+
+        with col1:
+            st.success("Original Text")
+            st.write(raw_text)
+
+            st.success("Prediction")
+            emoji_icon = emotions_dir[prediction]
+            st.write("{} : {}".format(prediction, emoji_icon))
+            st.write("Confidence : {}".format(np.max(probability)))
+
+        with col2:
+            st.success("Prediction Probability")
+            proba_df = pd.DataFrame(probability, columns=pipe_lr.classes_)
+            proba_df_clean = proba_df.T.reset_index()
+            proba_df_clean.columns = ["emotions", "probability"]
+
+            fig = alt.Chart(proba_df_clean).mark_bar().encode(x='emotions', y='probability', color='emotions')
+            st.altair_chart(fig, use_container_width=True)
+
+if __name__ == '__main__':
+    main()
